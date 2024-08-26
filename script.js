1a1d40b79d356dd4c1cbba145c9aed176adb69ed
AliYagmurGames <ravenperception@gmail.com>
2024-08-26 23:29:36 +0300

    Some trials on how to get user input for movement

--- script.js
+++ script.js
@@ -5,15 +5,45 @@ const CANVAS_WIDTH = canvas.width = 1200;
 const CANVAS_HEIGHT = canvas.height = 700;
 
 let gameFrame = 0;
-const staggerFrames = 3;
+const staggerFrames = 8;
+
+let xPos = 0;
+let yPos = 0;
 
 function animate() {
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.fillRect(10, 10, 50, 50);
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.fillRect(xPos, yPos, 50, 50);
 
-  //ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    if (gameFrame % staggerFrames === 0) {
+        document.addEventListener('keydown', (e) => {
+            if (e.code === 'ArrowUp') {
+                if (yPos > 1) {
+                    yPos -= 1;
+                }
+            }
+            if (e.code === 'ArrowDown') {
+                if (yPos < CANVAS_HEIGHT - 50) {
+                    yPos += 1;
+                }
+            }
+            if (e.code === 'ArrowLeft') {
+                if (xPos > 1) {
+                    xPos -= 1;
+                }
+            }
+            if (e.code === 'ArrowRight') {
+                if (xPos < CANVAS_WIDTH - 50) {
+                    xPos += 2;
+                }
+            }
+        });
 
-  gameFrame++;
-  requestAnimationFrame(animate);
+    }
+    
+
+    //ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+
+    gameFrame++;
+    requestAnimationFrame(animate);
 }
 animate();
